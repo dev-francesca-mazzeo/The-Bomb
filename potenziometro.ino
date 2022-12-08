3683e660173c9fb3fe5f3c6b92bd50ec8bca271a
dev.francesca.mazzeo <106004978+dev-francesca-mazzeo@users.noreply.github.com>
2022-12-08 12:24:11 +0100

    Add files via upload

---
+++ potenziometro.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  stampaCiao();
+
+}
+
+void potenziometroLed() {
+    //In questa funzione è contenuto il codice inerente al potenziometro
+    Serial.println("Funzione potenziometro!");
+}  //fine del blocco di istruzioni
