e67fbd12b501f517ff186cf83f5f58450409e1a9
dev.francesca.mazzeo <106004978+dev-francesca-mazzeo@users.noreply.github.com>
2022-12-08 12:23:13 +0100

    Add files via upload

---
+++ keypad.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  stampaCiao();
+
+}
+
+void keyPad() {
+    //In questa funzione è contenuto il codice inerente al keypad
+    Serial.println("Funzione Keypad!");
+}  //fine del blocco di istruzioni
